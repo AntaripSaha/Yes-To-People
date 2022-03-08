16165fafc9abf21910f2408c883a140e00658c43
AntaripSaha <antarip15@gmail.com>
2022-03-08 17:40:43 +0600

    08_03_22 last update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\HomePageController;
+use App\Http\Controllers\frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Auth::routes();
+
+Route::any('/', [HomeController::class, 'index'])->name('Home');
 
 Route::prefix('admin')->middleware('admin')->group(function(){
 
