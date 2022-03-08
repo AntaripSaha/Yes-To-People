9b5c66b0d9b09a9d09258e1890b9cd4b6bbdeb49
AntaripSaha <antarip15@gmail.com>
2022-03-08 16:07:57 +0600

    Authentication Added

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->middleware('admin')->group(function(){
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('admin.home');
+
+});
+
